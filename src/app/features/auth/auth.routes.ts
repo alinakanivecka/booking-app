e323982f98d3cf427e139fa80974371330f1a3cb
Alina Kanivetska <alinakaniveckaa@gmail.com>
2026-06-05 17:10:07 +0200

    BCA-6: Complete user registration

---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { Register } from "./pages/register/register";
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    component: Register
+  }
+]
